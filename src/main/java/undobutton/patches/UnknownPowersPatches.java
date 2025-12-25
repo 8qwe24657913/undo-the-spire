95c10f47105c3eb31ec51e4eceb54f942688a0e4
8q <yggdfolium@gmail.com>
2025-12-25 20:56:25 +0800

    Fix game crash when `Power.<init>(AbstractCreature, int)` does not exist

--- src/main/java/undobutton/patches/UnknownPowersPatches.java
+++ src/main/java/undobutton/patches/UnknownPowersPatches.java
@@ -9,6 +9,7 @@ import javassist.CtBehavior;
 import savestate.powers.PowerState;
 import undobutton.UndoButtonMod;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class UnknownPowersPatches {
@@ -16,20 +17,27 @@ public class UnknownPowersPatches {
     public static class forPowerPatch {
         @SpireInsertPatch(locator = Locator.class)
         public static SpireReturn<PowerState> tryToSaveUnknownPower(AbstractPower power) {
-            if (!UndoButtonMod.DEBUG) {
-                return SpireReturn.Return(new PowerState(power) {
-                    @Override
-                    public AbstractPower loadPower(AbstractCreature targetAndSource) {
-                        try {
-                            return power.getClass().getConstructor(AbstractCreature.class, int.class).newInstance(targetAndSource, amount);
-                        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-                                 NoSuchMethodException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                });
+            if (UndoButtonMod.DEBUG) {
+                return SpireReturn.Continue();
             }
-            return SpireReturn.Continue();
+            Class<? extends AbstractPower> clazz = power.getClass();
+            Constructor<? extends AbstractPower> constructor;
+            try {
+                constructor = clazz.getConstructor(AbstractCreature.class, int.class);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException("Failed to generate default PowerState. " + clazz.getName() + ".<init>(AbstractCreature, int) does not exist.");
+            }
+            return SpireReturn.Return(new PowerState(power) {
+                @Override
+                public AbstractPower loadPower(AbstractCreature targetAndSource) {
+                    UndoButtonMod.logger.debug("Try loading {} with default PowerState, which may cause compatibility issues.", clazz.getName());
+                    try {
+                        return constructor.newInstance(targetAndSource, amount);
+                    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
         }
 
         public static class Locator extends SpireInsertLocator {
